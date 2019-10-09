b7acfa8c59229b56328e5b0f2614af79e6f3138f
Jim de Beer <beerdejim@gmail.com>
2019-10-09 17:57:38 +0200

    fix

--- test/index.js
+++ test/index.js
@@ -18,6 +18,13 @@ getPackagePath(process.cwd(), true)
     console.log(Error)
   })
 
+/*
+cannot find parsed browser /Users/jim/saulx/v2/apps/app/node_modules/react/cjs/react.development
+cannot find parsed browser /Users/jim/saulx/v2/apps/app/node_modules/react-dom/cjs/react-dom.development
+cannot find parsed browser /Users/jim/saulx/v2/apps/app/src/hub
+cannot find parsed browser /Users/jim/saulx/v2/apps/app/src/App
+*/
+
 getPackagePath(path.join(process.cwd(), 'test/server.js'))
   .then(v => {
     console.log(v)
