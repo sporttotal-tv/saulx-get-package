983e4ae798cb3bb1cb75038c5b638ee1e7d72e23
Jim de Beer <beerdejim@gmail.com>
2019-10-09 17:53:22 +0200

    fix

--- test/index.js
+++ test/index.js
@@ -1,4 +1,6 @@
 const findPkg = require('../lib')
+const { getPackagePath } = findPkg
+const path = require('path')
 
 findPkg().then(v => {
   console.log(v)
@@ -7,3 +9,19 @@ findPkg().then(v => {
 findPkg(process.cwd(), true).then(v => {
   console.log(v)
 })
+
+getPackagePath(process.cwd(), true)
+  .then(v => {
+    console.log(v)
+  })
+  .catch(err => {
+    console.log(Error)
+  })
+
+getPackagePath(path.join(process.cwd(), 'test/server.js'))
+  .then(v => {
+    console.log(v)
+  })
+  .catch(err => {
+    console.log(Error)
+  })
